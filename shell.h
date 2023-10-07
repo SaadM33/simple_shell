fcff95c9033d2dcb2fad37eb1d9352899e5a1882
SaadM33 <119438070+SaadM33@users.noreply.github.com>
2023-10-07 17:29:07 +0100

    Create shell.h

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+extern char **environ;
+
+#endif
