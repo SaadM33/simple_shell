d5dbfefa4045750ae3479098ca7943ac42bb5bc5
SaadM33 <119438070+SaadM33@users.noreply.github.com>
2023-10-11 23:41:28 +0100

    added print_error and _itoa

--- shell.h
+++ shell.h
@@ -19,16 +19,19 @@ extern char **environ;
 
 /*==========Macros========================*/
 #define WRITE(c) (write(STDOUT_FILENO, c, _strlen(c)))
+
 /*==========Functions prototypes==========*/
 char *read_line(void);
 char **divider(char *line);
 void free_array_of_strings(char **arr);
 int _execute(char **cmd, char *input);
+void print_error(char *input, int counter, char argv);
 
 /*==========Strings prototypes==========*/
 char *_strdup(const char *str);
 int _strcmp(char *s1, char *s2);
 int _strlen(char *s);
 char *_strcat(char *dest, char *src);
+char *_itoa(unsigned int n);
 
 #endif /* SHELL_H */
