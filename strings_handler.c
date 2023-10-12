4a0841181e96c475f4b3cefadc26a8c20e50961e
SaadM33 <119438070+SaadM33@users.noreply.github.com>
2023-10-12 21:35:38 +0100

    betty stuff

--- strings_handler.c
+++ strings_handler.c
@@ -9,23 +9,21 @@
  */
 char *_strdup(const char *str)
 {
-	char *ptr;
-	int i, len = 0;
+    char *ptr;
+    int i, len = 0;
 
-	if (str == NULL)
-		return (NULL);
-	while (*str != '\0')
-	{
-		len++;
-		str++;
-	}
-	str = str - len;
-	ptr = malloc(sizeof(char) * (len + 1));
-	if (ptr == NULL)
-		return (NULL);
-	for (i = 0; i <= len; i++)
-		ptr[i] = str[i];
-	return (ptr);
+    if (str == NULL)
+        return (NULL);
+    while (str[len] != '\0')
+    {
+        len++;
+    }
+    ptr = malloc(sizeof(char) * (len + 1));
+    if (ptr == NULL)
+        return (NULL);
+    for (i = 0; i <= len; i++)
+        ptr[i] = str[i];
+    return (ptr);
 }
 
 /**
@@ -38,18 +36,19 @@ char *_strdup(const char *str)
  */
 int _strcmp(char *s1, char *s2)
 {
-	int cmp;
-	cmp = (int)*s1 - (int)*s2;
+    int cmp;
 
-	while (*s1)
-	{
-		if (*s1 != *s2)
-			break;
-		s1++;
-		s2++;
-		cmp = (int)*s1 - (int)*s2;
-	}
-	return (cmp);
+    cmp = (int)*s1 - (int)*s2;
+
+    while (*s1)
+    {
+        if (*s1 != *s2)
+            break;
+        s1++;
+        s2++;
+        cmp = (int)*s1 - (int)*s2;
+    }
+    return (cmp);
 }
 
 /**
@@ -60,11 +59,11 @@ int _strcmp(char *s1, char *s2)
  */
 int _strlen(char *s)
 {
-	int len = 0;
+    int len = 0;
 
-	while (s[len])
-		len++;
-	return (len);
+    while (s[len])
+        len++;
+    return (len);
 }
 
 /**
@@ -76,17 +75,16 @@ int _strlen(char *s)
  */
 char *_strcat(char *dest, char *src)
 {
-	char *p = dest;
+    char *p = dest;
 
-	while (*p)
-		p++;
-	while (*src)
-	{
-		*p = *src;
-		p++;
-		src++;
-	}
-	*p = *src;
-	return (dest);
+    while (*p)
+        p++;
+    while (*src)
+    {
+        *p = *src;
+        p++;
+        src++;
+    }
+    *p = *src;
+    return (dest);
 }
-
