49d3511278f607313b7db8747b1c2d55a62c0dc9
SaadM33 <119438070+SaadM33@users.noreply.github.com>
2023-10-15 00:37:31 +0100

    Update built-in.c

--- built-in.c
+++ built-in.c
@@ -41,3 +41,30 @@ int  env_built_in(void)
 	}
 	return (0);
 }
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ * Return: The integer representation of the string, or 0 if the string does
+ * not contain any valid numeric characters.
+ */
+int _atoi(char *s)
+{
+    int result = 0;
+    int sign = 1;
+    int i = 0;
+
+    while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
+        i++;
+    if (s[i] == '-' || s[i] == '+')
+    {
+        sign = (s[i] == '-') ? -1 : 1;
+        i++;
+    }
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        result = (result * 10) + (s[i] - '0');
+        i++;
+    }
+    return result * sign;
+}
