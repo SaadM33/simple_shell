a45feeb9ed63d1b9640a00d1faa03d03fce90dc7
SaadM33 <119438070+SaadM33@users.noreply.github.com>
2023-10-15 01:13:17 +0100

    Update strings_handler.c

--- strings_handler.c
+++ strings_handler.c
@@ -34,7 +34,6 @@ char *_strdup(const char *str)
  * Return: An integer less than, equal to, or greater than zero if s1 is
  * found, respectively, to be less than, to match, or be greater than s2.
  */
-/**
 
 int _strcmp(char *s1, char *s2)
 {
